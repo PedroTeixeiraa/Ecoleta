1af79b0a26da3f96aab87a9297c5a084d5efc766
Pedro Teixeira Alves <pedroteixeiraalves007@gmail.com>
2020-06-05 15:04:17 -0300

    feat(web): rotas da aplicação

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import './App.css'
 
-import Home from './pages/Home'
+import Routes from './routes'
 
 function App() {
   return (
-    <Home />
-    
+    <Routes />
   )
 }
 
--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { FiLogIn } from 'react-icons/fi'
 
 import './styles.css'
@@ -17,12 +18,12 @@ const Home = () => {
                     <h1>Seu marketplace de coleta de resíduos.</h1>
                     <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
 
-                    <a href="/cadastro">
+                    <Link to="/create-point">
                         <span>
                             <FiLogIn />
                         </span>
                         <strong>Cadastre um ponto de coleta</strong>
-                    </a>
+                    </Link>
                 </main>
             </div>
         </div>
