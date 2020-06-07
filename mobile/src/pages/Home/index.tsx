937bddd611db3793e8ef2dd82751c203db7dfdd2
Pedro Teixeira Alves <pedroteixeiraalves007@gmail.com>
2020-06-06 21:48:59 -0300

    feat: busca de UF e cidade

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,41 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Feather as Icon } from '@expo/vector-icons'
-import { View, Text, Image, ImageBackground, StyleSheet } from 'react-native'
+import { View, Text, Image, ImageBackground, StyleSheet, TextInput, KeyboardAvoidingView, Platform } from 'react-native'
 import { RectButton } from 'react-native-gesture-handler'
 import { useNavigation } from '@react-navigation/native'
 
 const Home = () => {
+    const [uf, setUf] = useState('')
+    const [city, setCity] = useState('') 
     const navigation = useNavigation()
 
     function handleNavigateToPoints() {
-      navigation.navigate('Points')
+      navigation.navigate('Points', {
+        city,
+        uf
+      })
     }
     
     return (
-        <ImageBackground 
-            source={require('../../assets/home-background.png')} 
-            style={styles.container}
-            imageStyle={{ width: 274, height: 368 }}
+        <KeyboardAvoidingView 
+            behavior='height'
+            style={{ flex: 1 }}
         >
-            <View style={styles.main}>
-                <Image source={require('../../assets/logo.png')}/>
-                <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
-                <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</Text>
-            </View>
-
-            <View style={styles.footer}>
-                <RectButton style={styles.button} onPress={handleNavigateToPoints}>
-                    <View style={styles.buttonIcon}>
-                        <Text>
-                            <Icon name="arrow-right" color="#FFF" size={24}/>
-                        </Text>
+            <ImageBackground 
+                source={require('../../assets/home-background.png')} 
+                style={styles.container}
+                imageStyle={{ width: 274, height: 368 }}
+            >
+                <View style={styles.main}>
+                    <Image source={require('../../assets/logo.png')}/>
+                    <View>
+                        <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
+                        <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</Text>
                     </View>
-                    <Text style={styles.buttonText}>
-                        Entrar
-                    </Text>
-                </RectButton>
-            </View>
-        </ImageBackground>
+                </View>
+
+                <View style={styles.footer}>
+
+                    <TextInput 
+                        style={styles.input} 
+                        placeholder="Digite a UF"
+                        value={uf}
+                        onChangeText={setUf}
+                        autoCapitalize="characters"
+                        autoCorrect={false}
+                        maxLength={2}
+                    />
+                    <TextInput 
+                        style={styles.input}
+                        placeholder="Digite a cidade"
+                        value={city}
+                        autoCorrect={false}
+                        onChangeText={setCity}
+                    />
+                    <RectButton style={styles.button} onPress={handleNavigateToPoints}>
+                        <View style={styles.buttonIcon}>
+                            <Text>
+                                <Icon name="arrow-right" color="#FFF" size={24}/>
+                            </Text>
+                        </View>
+                        <Text style={styles.buttonText}>
+                            Entrar
+                        </Text>
+                    </RectButton>
+                </View>
+            </ImageBackground>
+        </KeyboardAvoidingView>
     )
 }
 
--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { View, Image, StyleSheet, TouchableOpacity, Text, ScrollView, Alert} from 'react-native'
 import { Feather as Item } from '@expo/vector-icons'
 import Constants from 'expo-constants'
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, useRoute } from '@react-navigation/native'
 import MapView, { Marker } from 'react-native-maps'
 import { SvgUri } from 'react-native-svg'
 import * as Location from 'expo-location'
@@ -22,6 +22,11 @@ interface Point {
   longitude: number 
 }
 
+interface Params {
+  city: string
+  uf: string
+}
+
 const Points = () => {
     const [items, setItems] = useState<Item[]>([])
     const [points, setPoints] = useState<Point[]>([])
@@ -30,6 +35,9 @@ const Points = () => {
     const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0])
 
     const navigation = useNavigation()
+    const route = useRoute()
+
+    const routeParams = route.params as Params
 
     useEffect(() => {
       async function loadLocation() {
@@ -60,14 +68,14 @@ const Points = () => {
     useEffect(() => {
       api.get('points', {
         params: {
-          city: 'Esteio',
-          uf: 'RS',
-          items: [1, 2]
+          city: routeParams.city,
+          uf: routeParams.uf,
+          items: selectedItems
         }
       }).then(response => {
         setPoints(response.data)
       })
-    }, [])
+    }, [selectedItems])
 
     function handleNavigateToHome() {
         navigation.goBack()
