0d3408c1d27539d819c5a6e8263570541ff997da
Pedro Teixeira Alves <pedroteixeiraalves007@gmail.com>
2020-06-12 11:38:05 -0300

    feat(mobile): estilos da página de detalhes

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,169 +1,98 @@
 import React, { useState, useEffect } from 'react'
-import { View, StyleSheet, TouchableOpacity, Image, Text, Linking } from 'react-native'
-import Constants from 'expo-constants'
-import { Feather as Icon, FontAwesome,  } from '@expo/vector-icons'
+import { View, TouchableOpacity, Image, Text, Linking } from 'react-native'
+import { Feather as Icon, FontAwesome, } from '@expo/vector-icons'
 import { useNavigation, useRoute } from '@react-navigation/native'
 import { RectButton } from 'react-native-gesture-handler'
 import * as MailComposer from 'expo-mail-composer'
+
+import styles from './styles'
 import api from '../../services/api'
 
 interface Params {
-    point_id: number
+  point_id: number
 }
 
 interface Data {
-    point: {
-      image: string
-      image_url: string
-      name: string
-      email: string
-      whatsapp: string
-      city: string
-      uf: string
-    }
-    item: {
-      title: string
-    }[]
+  serializedPoint: {
+    image: string
+    image_url: string
+    name: string
+    email: string
+    whatsapp: string
+    city: string
+    uf: string
+  }
+  item: {
+    title: string
+  }[]
 }
 
 const Detail = () => {
-    const [data, setData] = useState<Data>({} as Data)
+  const [data, setData] = useState<Data>({} as Data)
 
-    const navigation = useNavigation()
-    const route = useRoute()
+  const navigation = useNavigation()
+  const route = useRoute()
 
-    const routeParams = route.params as Params
+  const routeParams = route.params as Params
+  
+  useEffect(() => {
+    api.get(`points/${routeParams.point_id}`).then(response => {
+      console.log(response.data)
+      setData(response.data)
+    })
+  }, [])
 
-    useEffect(() => {
-        api.get(`points/${routeParams.point_id}`).then(response => {
-          setData(response.data)
-        }) 
-    }, [])
+  function handleNavigateToPoints() {
+    navigation.goBack()
+  }
 
-    function handleNavigateToPoints() {
-        navigation.goBack()
-    }
+  function handleComposeMail() {
+    MailComposer.composeAsync({
+      subject: 'Interesse na coleta de resíduos',
+      recipients: [data.serializedPoint.email],
+    })
+  }
 
-    function handleComposeMail() {
-        MailComposer.composeAsync({
-            subject: 'Interesse na coleta de resíduos',
-            recipients: [data.point.email],         
-        })
-    }
+  function handleWhatsapp() {
+    Linking.openURL(`whatsapp://send?phone=${data.serializedPoint.whatsapp}&text=Tenho Interesse sobre coleta de resíduos`)
+  }
 
-    function handleWhatsapp() {
-        Linking.openURL(`whatsapp://send?phone=${data.point.whatsapp}&text=Tenho Interesse sobre coleta de resíduos`)
-    }
+  if (!data.serializedPoint) {
+    return null
+  }
 
-    if (!data.point) {
-      return null
-    }
-    
-    return (
-        <>
-            <View style={styles.container}>
-                <TouchableOpacity onPress={handleNavigateToPoints}>
-                    <Icon name="arrow-left" size={25} color="#34cb79"/>
-                </TouchableOpacity>
+  return (
+    <>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleNavigateToPoints}>
+          <Icon name="arrow-left" size={25} color="#34cb79" />
+        </TouchableOpacity>
 
-                <Image style={styles.pointImage} source={{uri: data.point.image_url}}/>
-                
-                <Text style={styles.pointName}>{data.point.name}</Text>
-                <Text style={styles.pointItems}>
-                   {data.item.map(item => item.title).join(', ')}
-                </Text>
+        <Image style={styles.pointImage} source={{ uri: data.serializedPoint.image_url }} />
 
-                <View style={styles.address}>
-                    <Text style={styles.addressTitle}>Endereço</Text>
-                    <Text style={styles.addressContent}>{data.point.city}, {data.point.uf}</Text>
-                </View>
-            </View>
-            <View style={styles.footer}> 
-                <RectButton style={styles.button} onPress={handleWhatsapp}>
-                    <FontAwesome name="whatsapp" size={20} color="#fff"/>
-                    <Text style={styles.buttonText}>Whatsapp</Text>
-                </RectButton>
+        <Text style={styles.pointName}>{data.serializedPoint.name}</Text>
+        <Text style={styles.pointItems}>
+          {data.item.map(item => item.title).join(', ')}
+        </Text>
 
-                <RectButton style={styles.button} onPress={handleComposeMail}>
-                    <Icon name="mail" size={20} color="#fff"/>
-                    <Text style={styles.buttonText}>E-mail</Text>
-                </RectButton>
-            </View>
-        </>
-    )
+        <View style={styles.address}>
+          <Text style={styles.addressTitle}>Endereço</Text>
+          <Text style={styles.addressContent}>{data.serializedPoint.city}, {data.serializedPoint.uf}</Text>
+        </View>
+      </View>
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={handleWhatsapp}>
+          <FontAwesome name="whatsapp" size={20} color="#fff" />
+          <Text style={styles.buttonText}>Whatsapp</Text>
+        </RectButton>
+
+        <RectButton style={styles.button} onPress={handleComposeMail}>
+          <Icon name="mail" size={20} color="#fff" />
+          <Text style={styles.buttonText}>E-mail</Text>
+        </RectButton>
+      </View>
+    </>
+  )
 }
 
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 32,
-      paddingTop: 20 + Constants.statusBarHeight,
-    },
-  
-    pointImage: {
-      width: '100%',
-      height: 120,
-      resizeMode: 'cover',
-      borderRadius: 10,
-      marginTop: 32,
-    },
-  
-    pointName: {
-      color: '#322153',
-      fontSize: 28,
-      fontFamily: 'Ubuntu_700Bold',
-      marginTop: 24,
-    },
-  
-    pointItems: {
-      fontFamily: 'Roboto_400Regular',
-      fontSize: 16,
-      lineHeight: 24,
-      marginTop: 8,
-      color: '#6C6C80'
-    },
-  
-    address: {
-      marginTop: 32,
-    },
-    
-    addressTitle: {
-      color: '#322153',
-      fontFamily: 'Roboto_500Medium',
-      fontSize: 16,
-    },
-  
-    addressContent: {
-      fontFamily: 'Roboto_400Regular',
-      lineHeight: 24,
-      marginTop: 8,
-      color: '#6C6C80'
-    },
-  
-    footer: {
-      borderTopWidth: StyleSheet.hairlineWidth,
-      borderColor: '#999',
-      paddingVertical: 20,
-      paddingHorizontal: 32,
-      flexDirection: 'row',
-      justifyContent: 'space-between'
-    },
-    
-    button: {
-      width: '48%',
-      backgroundColor: '#34CB79',
-      borderRadius: 10,
-      height: 50,
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center'
-    },
-  
-    buttonText: {
-      marginLeft: 8,
-      color: '#FFF',
-      fontSize: 16,
-      fontFamily: 'Roboto_500Medium',
-    },
-  });
 export default Detail
