3a1602cad4d638545b3733be8663e604f046c638
Pedro Teixeira Alves <pedroteixeiraalves007@gmail.com>
2020-06-12 11:39:49 -0300

    feat(mobile): estilos página de Cadastro de pontos de coleta

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react'
-import { View, Image, StyleSheet, TouchableOpacity, Text, ScrollView, Alert} from 'react-native'
+import { View, Image, TouchableOpacity, Text, ScrollView, Alert} from 'react-native'
 import { Feather as Item } from '@expo/vector-icons'
-import Constants from 'expo-constants'
 import { useNavigation, useRoute } from '@react-navigation/native'
 import MapView, { Marker } from 'react-native-maps'
 import { SvgUri } from 'react-native-svg'
 import * as Location from 'expo-location'
+
+import styles from './styles'
 import api from '../../services/api'
 
 interface Item {
@@ -74,6 +75,7 @@ const Points = () => {
           items: selectedItems
         }
       }).then(response => {
+        console.log(response.data)
         setPoints(response.data)
       })
     }, [selectedItems])
@@ -132,9 +134,9 @@ const Points = () => {
                                     <View style={styles.mapMarkerContainer}>
                                         <Image
                                             style={styles.mapMarkerImage} 
-                                            source={{ uri: point.image}}
+                                            source={{ uri: point.image_url}}
                                         />
-                                        <Text style={styles.mapMarkerTitle}>{point.image_url}</Text>
+                                        <Text style={styles.mapMarkerTitle}>{point.name}</Text>
                                     </View>
                                 </Marker>
                             ))}
@@ -162,109 +164,10 @@ const Points = () => {
                             <Text style={styles.itemTitle}>{item.title}</Text>
                         </TouchableOpacity>
                     ))}
-                    
-
                 </ScrollView>
             </View>
         </>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      paddingHorizontal: 32,
-      paddingTop: 20 + Constants.statusBarHeight,
-    },
-  
-    title: {
-      fontSize: 20,
-      fontFamily: 'Ubuntu_700Bold',
-      marginTop: 24,
-    },
-  
-    description: {
-      color: '#6C6C80',
-      fontSize: 16,
-      marginTop: 4,
-      fontFamily: 'Roboto_400Regular',
-    },
-  
-    mapContainer: {
-      flex: 1,
-      width: '100%',
-      borderRadius: 10,
-      overflow: 'hidden',
-      marginTop: 16,
-    },
-  
-    map: {
-      width: '100%',
-      height: '100%',
-    },
-  
-    mapMarker: {
-      width: 90,
-      height: 80, 
-    },
-  
-    mapMarkerContainer: {
-      width: 90,
-      height: 70,
-      backgroundColor: '#34CB79',
-      flexDirection: 'column',
-      borderRadius: 8,
-      overflow: 'hidden',
-      alignItems: 'center'
-    },
-  
-    mapMarkerImage: {
-      width: 90,
-      height: 45,
-      resizeMode: 'cover',
-    },
-  
-    mapMarkerTitle: {
-      flex: 1,
-      fontFamily: 'Roboto_400Regular',
-      color: '#FFF',
-      fontSize: 13,
-      lineHeight: 23,
-    },
-  
-    itemsContainer: {
-      flexDirection: 'row',
-      marginTop: 16,
-      marginBottom: 32,
-    },
-  
-    item: {
-      backgroundColor: '#fff',
-      borderWidth: 2,
-      borderColor: '#eee',
-      height: 120,
-      width: 120,
-      borderRadius: 8,
-      paddingHorizontal: 16,
-      paddingTop: 20,
-      paddingBottom: 16,
-      marginRight: 8,
-      alignItems: 'center',
-      justifyContent: 'space-between',
-  
-      textAlign: 'center',
-    },
-  
-    selectedItem: {
-      borderColor: '#34CB79',
-      borderWidth: 2,
-    },
-  
-    itemTitle: {
-      fontFamily: 'Roboto_400Regular',
-      textAlign: 'center',
-      fontSize: 13,
-    },
-  })
 
 export default Points
